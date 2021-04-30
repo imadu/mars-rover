e4203e0178e9d3ad0681787ce4eab2dbd1941f26
Ikechukwu Madu <ikechukwu.madu1@gmail.com>
2021-04-30 18:35:55 +0100

    Patch: Rover initialization Chore: Add code to ensure rover initialization with arguments pass

--- src/rover.ts
+++ src/rover.ts
@@ -1,5 +1,10 @@
 export class MarsRover {
-    constructor(){
-        
+    xAxis: number;
+    yAxis: number;
+    direction: string;
+    constructor(xAxis: number, yAxis: number, direction:string){
+        this.xAxis = xAxis;
+        this.yAxis = yAxis;
+        this.direction = direction;  
     }
 }
