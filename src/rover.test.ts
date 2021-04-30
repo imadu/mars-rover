447ef7a724cfae4e55b349795466969a98630301
Ikechukwu Madu <ikechukwu.madu1@gmail.com>
2021-04-30 18:35:06 +0100

    Patch: Rover Initialization Chore: Add test to initialize rover with arguments

--- src/rover.test.ts
+++ src/rover.test.ts
@@ -2,7 +2,9 @@ import {MarsRover} from './rover'
 
 describe('Mars Rover', () => {
     it('should initialize mars rover', ()=> {
-        const rover = new MarsRover();
-        expect(rover).toBeTruthy();
+        const rover = new MarsRover(4, 2, 'EAST');
+        expect(rover.xAxis).toBe(4);
+        expect(rover.yAxis).toBe(2);
+        expect(rover.direction).toBe('EAST');
     })
 })
