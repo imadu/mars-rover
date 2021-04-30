651322676b5fefaa55f322039833c70342c827c8
Ikechukwu Madu <ikechukwu.madu1@gmail.com>
2021-04-30 19:14:36 +0100

    Patch: Rover Commands Chore: Added test to throw error when rover receives an illegal command

--- src/rover.test.ts
+++ src/rover.test.ts
@@ -1,10 +1,18 @@
-import {MarsRover} from './rover'
+import { MarsRover } from "./rover";
 
-describe('Mars Rover', () => {
-    it('should initialize mars rover', ()=> {
-        const rover = new MarsRover(4, 2, 'EAST');
-        expect(rover.xAxis).toBe(4);
-        expect(rover.yAxis).toBe(2);
-        expect(rover.direction).toBe('EAST');
-    })
-})
+describe("Mars Rover", () => {
+  it("should initialize mars rover", () => {
+    const rover = new MarsRover(4, 2, "EAST");
+    expect(rover).toBeTruthy();
+    expect(rover.xAxis).toBe(4);
+    expect(rover.yAxis).toBe(2);
+    expect(rover.direction).toBe("EAST");
+  });
+
+  it("should throw an invalid command error", () => {
+    const rover = new MarsRover(4, 2, "EAST");
+    expect(() => rover.MoveRover("FGFFFRLB")).toThrow(
+      Error
+    );
+  });
+});
