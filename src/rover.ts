b239b59814ee38183d1a6785cab78df228de9d74
Ikechukwu Madu <ikechukwu.madu1@gmail.com>
2021-04-30 19:15:21 +0100

    Patch: Move Rover commands Chore: Added method to  `MoveRover` and check to throw error if rover receives illegal command

--- src/rover.ts
+++ src/rover.ts
@@ -1,10 +1,28 @@
 export class MarsRover {
-    xAxis: number;
-    yAxis: number;
-    direction: string;
-    constructor(xAxis: number, yAxis: number, direction:string){
-        this.xAxis = xAxis;
-        this.yAxis = yAxis;
-        this.direction = direction;  
+  xAxis: number;
+  yAxis: number;
+  direction: string;
+  constructor(xAxis: number, yAxis: number, direction: string) {
+    this.xAxis = xAxis;
+    this.yAxis = yAxis;
+    this.direction = direction;
+  }
+
+  MoveRover(command: string) {
+    const moveCommands: {[key: string]:string} = {
+      F: "MOVE FORWARD",
+      B: "MOVE BACKWARD",
+      L: "ROTATE LEFT",
+      R: "ROTATE RIGHT",
+    };
+    const commands = command.split("");
+    //check for illegal character commands and throw an error
+    for (let i = 0; i < commands.length; i++) {
+        const element = commands[i];
+        if(!moveCommands[element]){
+            throw new Error(`Invalid command ${element} in command string`)
+        }
+        
     }
+  }
 }
