739a0919128a8c6f0a81cf810c978dc0d3770b75
Ikechukwu Madu <ikechukwu.madu1@gmail.com>
2021-05-02 00:18:24 +0100

    Patch: Move Rover Chore: Added function to move rover Chore Added nextDirection Map for location states Chore Added nextState function to return new state for rover every time it moves

--- src/rover.ts
+++ src/rover.ts
@@ -1,28 +1,89 @@
-export class MarsRover {
-  xAxis: number;
-  yAxis: number;
-  direction: string;
-  constructor(xAxis: number, yAxis: number, direction: string) {
-    this.xAxis = xAxis;
-    this.yAxis = yAxis;
-    this.direction = direction;
+export enum Direction {
+  NORTH = "NORTH",
+  SOUTH = "SOUTH",
+  EAST = "EAST",
+  WEST = "WEST",
+}
+
+export interface State {
+  x: number;
+  y: number;
+  direction: Direction;
+}
+
+const nextDirection = new Map([
+  [`${Direction.NORTH}-L`, Direction.WEST],
+  [`${Direction.SOUTH}-L`, Direction.EAST],
+  [`${Direction.WEST}-L`, Direction.SOUTH],
+  [`${Direction.EAST}-L`, Direction.NORTH],
+  [`${Direction.NORTH}-R`, Direction.EAST],
+  [`${Direction.SOUTH}-R`, Direction.WEST],
+  [`${Direction.WEST}-R`, Direction.NORTH],
+  [`${Direction.EAST}-R`, Direction.SOUTH],
+]);
+
+function nextState(command: string, state: State): State {
+  //treat for both L, R, F and B commands
+  if (command === "L" || command === "R") {
+    const newDirection = nextDirection.get(`${state.direction}-${command}`);
+    if (newDirection) {
+      return {
+        ...state,
+        direction: newDirection,
+      };
+    }
+    throw new Error(`Invalid command found in Direction ${command}`)
   }
 
-  MoveRover(command: string) {
-    const moveCommands: {[key: string]:string} = {
-      F: "MOVE FORWARD",
-      B: "MOVE BACKWARD",
-      L: "ROTATE LEFT",
-      R: "ROTATE RIGHT",
-    };
+  if (state.direction === Direction.NORTH && command === "F") {
+    return { ...state, y: state.y + 1 };
+  }
+  if (state.direction === Direction.WEST && command === "F") {
+    return { ...state, x: state.x - 1 };
+  }
+  if (state.direction === Direction.EAST && command === "F") {
+    return { ...state, x: state.x + 1 };
+  }
+
+  if (state.direction === Direction.SOUTH && command === "F") {
+    return { ...state, y: state.y - 1 };
+  }
+
+  if (state.direction === Direction.NORTH && command === "B") {
+    return { ...state, y: state.y - 1 };
+  }
+
+  if (state.direction === Direction.SOUTH && command === "B") {
+    return { ...state, y: state.y - 1 };
+  }
+
+  if (state.direction === Direction.EAST && command === "B") {
+    return { ...state, x: state.x - 1 };
+  }
+
+  if (state.direction === Direction.WEST && command === "B") {
+    return { ...state, x: state.x - 1 };
+  }
+  //Throw new error 
+  throw new Error(`Invalid command ${command} found in command string`)
+}
+
+export class MarsRover {
+  state: State;
+  constructor(x: number, y: number, direction: Direction) {
+    this.state = { x, y, direction };
+  }
+
+  Move(command: string) {
     const commands = command.split("");
-    //check for illegal character commands and throw an error
+    let newState = this.state;
     for (let i = 0; i < commands.length; i++) {
-        const element = commands[i];
-        if(!moveCommands[element]){
-            throw new Error(`Invalid command ${element} in command string`)
-        }
-        
+      const command = commands[i];
+      //check for invalid character commands and throw an error
+      newState = nextState(command, newState);
     }
+    return `(${newState.x},${newState.y}) ${newState.direction}`
   }
 }
+
+
