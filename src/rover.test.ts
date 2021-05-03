2afdfe16e570a049a9fc4db5a6ce0067efac0157
Ikechukwu Madu <ikechukwu.madu1@gmail.com>
2021-05-03 05:09:59 +0100

    Patch: Move Rover Test Chore: Add test to ensure rover stops when it meets an obstacle Chore: Refactor tests for Rover initialization, movement and stop  (when obstacle is encountered) to use array of test cases Chore: Use `Direction` and `State` types for rover direction and states

--- src/rover.test.ts
+++ src/rover.test.ts
@@ -1,11 +1,11 @@
-import { Direction, MarsRover, State } from "./rover";
+import { Direction, MarsRover, Point, State } from "./rover";
 
-type TestCase = [State, string, string]
+type TestCase = [State, string, string];
+type SecondTestCase = [State, string, State, Point[]];
 
 describe("Mars Rover", () => {
   it("should initialize mars rover", () => {
-    var EAST: Direction = Direction[Direction.EAST]
-    const rover = new MarsRover(4, 2, EAST);
+    const rover = new MarsRover(4, 2, Direction.EAST);
     expect(rover).toBeTruthy();
     expect(rover.state.x).toBe(4);
     expect(rover.state.y).toBe(2);
@@ -13,34 +13,51 @@ describe("Mars Rover", () => {
   });
 
   it("should throw an invalid command error", () => {
-    var EAST: Direction = Direction[Direction.EAST]
-    const rover = new MarsRover(4, 2, EAST);
-    expect(() => rover.Move("FGFFFRLB")).toThrow(
-      Error
-    );
+    const rover = new MarsRover(4, 2, Direction.EAST);
+    expect(() => rover.Move("FGFFFRLB")).toThrow(Error);
   });
 
-  it("should move the rover and return coordinates", ()=> {
-    var EAST: Direction = Direction[Direction.EAST]
-    var NORTH: Direction = Direction[Direction.NORTH]
-    var WEST: Direction = Direction[Direction.WEST]
-    var SOUTH: Direction = Direction[Direction.SOUTH]
-   
+  it("should move the rover and return coordinates", () => {
+    
 
-      const testCases: TestCase[] = [
-        [{x: 4, y: 2, direction: EAST}, "FLFFFRFLB",  "(6,4) NORTH"],
-         [{x: 6, y: 4, direction: NORTH}, "FBBLFFLLRF", "(4,2) SOUTH"],
-        [{x: 5, y: 6, direction: SOUTH}, "BBBRFFLLBFF", "(4,3) EAST"],
-        [{x: 6, y: 4, direction: NORTH}, "FFFLLFFRRRRBB", "(6,3) SOUTH"],
-        [{x: 6, y: 4, direction: NORTH}, "LFFFRFLR", "(3,5) NORTH"]    
-      ]
+    const testCases: TestCase[] = [
+      [{ x: 4, y: 2, direction: Direction.EAST }, "FLFFFRFLB", "(6,4) NORTH"],
+      [{ x: 6, y: 4, direction: Direction.NORTH }, "FBBLFFLLRF", "(4,2) SOUTH"],
+      [{ x: 5, y: 6, direction: Direction.SOUTH }, "BBBRFFLLBFF", "(4,3) EAST"],
+      [{ x: 6, y: 4, direction: Direction.NORTH }, "FFFLLFFRRRRBB", "(6,3) SOUTH"],
+      [{ x: 6, y: 4, direction: Direction.NORTH }, "LFFFRFLR", "(3,5) NORTH"],
+    ];
 
-      for (let i = 0; i < testCases.length; i++) {
-          const [initalState, commands, expectedState]: TestCase = testCases[i] 
-         console.log(initalState)
-         const newRover = new MarsRover(initalState.x, initalState.y, initalState.direction)
-         console.log(newRover.Move(commands))
-         expect(newRover.Move(commands)).toBe(expectedState)
-      }
-  })
+    for (let i = 0; i < testCases.length; i++) {
+      const [initalState, commands, expectedState]: TestCase = testCases[i];
+      const newRover = new MarsRover(
+        initalState.x,
+        initalState.y,
+        initalState.direction
+      );
+      expect(newRover.Move(commands)).toBe(expectedState);
+    }
+  });
+
+  it("should return the previous state when  obstacle hit", () => {
+    const testCases: SecondTestCase[] = [
+      [{ x: 0, y: 3, direction: Direction.EAST }, "FLF", {x:1, y:3, direction: Direction.NORTH}, [[1,4]]],
+    ];
+
+    for (let i = 0; i < testCases.length; i++) {
+      const [
+        initalState,
+        commands,
+        finalState,
+        obstacle,
+      ]: SecondTestCase = testCases[i];
+      const newRover = new MarsRover(
+        initalState.x,
+        initalState.y,
+        initalState.direction,
+        obstacle
+      );
+      expect(newRover.Move(commands)).toBe(`(${finalState.x},${finalState.y}) ${finalState.direction} STOPPED`)
+    }
+  });
 });
