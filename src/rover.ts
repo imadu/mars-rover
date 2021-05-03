58eec2aa26626b9f277133dbab04ecf16008e8b9
Ikechukwu Madu <ikechukwu.madu1@gmail.com>
2021-05-03 05:12:35 +0100

    Patch; Stop Rover Chore: Add Rover stop  checker to stop rover if obstacle encountered Chore: Refactor code block to pass rover move and stop tests

--- src/rover.ts
+++ src/rover.ts
@@ -1,3 +1,5 @@
+export type Point = [number, number];
+
 export enum Direction {
   NORTH = "NORTH",
   SOUTH = "SOUTH",
@@ -11,7 +13,11 @@ export interface State {
   direction: Direction;
 }
 
-const nextDirection = new Map([
+/**
+ * NextDirection Map holds all the full combinations of change of direction of the State machine(i.e. the mars rover is a state machine)
+ */
+
+const NextDirection = new Map([
   [`${Direction.NORTH}-L`, Direction.WEST],
   [`${Direction.SOUTH}-L`, Direction.EAST],
   [`${Direction.WEST}-L`, Direction.SOUTH],
@@ -22,68 +28,66 @@ const nextDirection = new Map([
   [`${Direction.EAST}-R`, Direction.SOUTH],
 ]);
 
-function nextState(command: string, state: State): State {
-  //treat for both L, R, F and B commands
-  if (command === "L" || command === "R") {
-    const newDirection = nextDirection.get(`${state.direction}-${command}`);
-    if (newDirection) {
-      return {
-        ...state,
-        direction: newDirection,
-      };
-    }
-    throw new Error(`Invalid command found in Direction ${command}`)
-  }
-
-  if (state.direction === Direction.NORTH && command === "F") {
-    return { ...state, y: state.y + 1 };
-  }
-  if (state.direction === Direction.WEST && command === "F") {
-    return { ...state, x: state.x - 1 };
-  }
-  if (state.direction === Direction.EAST && command === "F") {
-    return { ...state, x: state.x + 1 };
-  }
-
-  if (state.direction === Direction.SOUTH && command === "F") {
-    return { ...state, y: state.y - 1 };
-  }
-
-  if (state.direction === Direction.NORTH && command === "B") {
-    return { ...state, y: state.y - 1 };
-  }
-
-  if (state.direction === Direction.SOUTH && command === "B") {
-    return { ...state, y: state.y - 1 };
-  }
-
-  if (state.direction === Direction.EAST && command === "B") {
-    return { ...state, x: state.x - 1 };
-  }
-
-  if (state.direction === Direction.WEST && command === "B") {
-    return { ...state, x: state.x - 1 };
-  }
-  //Throw new error 
-  throw new Error(`Invalid command ${command} found in command string`)
-}
+/**
+ * Delta Map holds all the full change of valuez of both x and y axis of the State machine
+ */
+const Delta = new Map([
+  [`${Direction.NORTH}-F`, [0, 1]],
+  [`${Direction.WEST}-F`, [-1, 0]],
+  [`${Direction.EAST}-F`, [1, 0]],
+  [`${Direction.SOUTH}-F`, [0, -1]],
+  [`${Direction.NORTH}-B`, [0, -1]],
+  [`${Direction.WEST}-B`, [1, 0]],
+  [`${Direction.EAST}-B`, [-1, 0]],
+  [`${Direction.SOUTH}-B`, [0, -1]],
+]);
 
 export class MarsRover {
   state: State;
-  constructor(x: number, y: number, direction: Direction) {
+  obstacles: Set<string>;
+
+  constructor(x: number, y: number, direction: Direction, obstacles?: Point[]) {
     this.state = { x, y, direction };
+    this.obstacles = obstacles ? new Set(obstacles.map(p => p.toString())): new Set();
   }
 
-  Move(command: string) {
-    const commands = command.split("");
-    let newState = this.state;
-    for (let i = 0; i < commands.length; i++) {
-      const command = commands[i];
-      //check for invalid character commands and throw an error
-      newState = nextState(command, newState);
+  private NextState(command: string, state: State): State {
+    //treat for both L, R, F and B commands
+    if (command === "L" || command === "R") {
+      const newDirection = NextDirection.get(`${state.direction}-${command}`);
+      if (newDirection) {
+        return {
+          ...state,
+          direction: newDirection,
+        };
+      }
+      throw new Error(`Invalid command found in Direction ${command}`);
     }
-    return `(${newState.x},${newState.y}) ${newState.direction}`
+
+    if (command === "F" || command === "B") {
+      const locationDelta = Delta.get(`${state.direction}-${command}`);
+      if (locationDelta) {
+        const newState = {
+          ...state,
+          x: state.x + locationDelta[0],
+          y: state.y + locationDelta[1],
+        };
+        return newState;
+      }
+    }
+    throw new Error(`Invalid command ${command} found in command string`);
+  }
+
+  Move(commands: string) {
+    let newState: State;
+    for (let i = 0; i < commands.length; i++) {
+      newState = this.NextState(commands[i], this.state);
+      if (this.obstacles.has(`${newState.x},${newState.y}`)) {
+          // We have a collision; stop processing commands:
+          return `(${this.state.x},${this.state.y}) ${this.state.direction} STOPPED`;
+      }
+      this.state = newState;
+    }
+    return `(${this.state.x},${this.state.y}) ${this.state.direction}`;
   }
 }
-
-
